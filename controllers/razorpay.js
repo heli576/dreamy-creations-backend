cea60903574d4b56150dcc317d7845a789170240
heli576 <helivakharia@gmail.com>
2020-10-01 22:41:39 +0530

    Add payment verification

--- controllers/razorpay.js
+++ controllers/razorpay.js
@@ -32,3 +32,24 @@ exports.processPayment=async(req,res)=>{
 		console.log(error)
 	}
 }
+
+exports.verification=(req,res)=>{
+	const secret=process.env.RAZORPAY_WEB_HOOK_SECRET;
+	console.log(req.body);
+	const crypto = require('crypto')
+
+	const shasum = crypto.createHmac('sha256', secret)
+	shasum.update(JSON.stringify(req.body))
+	const digest = shasum.digest('hex')
+
+	console.log(digest, req.headers['x-razorpay-signature'])
+
+	if (digest === req.headers['x-razorpay-signature']) {
+		console.log('request is legit')
+		// process it
+		require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
+	} else {
+		// pass it
+	}
+	res.json({status:'ok'});
+}
--- routes/razorpay.js
+++ routes/razorpay.js
@@ -2,12 +2,16 @@ const express=require("express");
 const router=express.Router();
 const {requireSignin,isAuth}=require("../controllers/auth");
 const {userById}=require("../controllers/user");
-const {processPayment}=require("../controllers/razorpay");
+const {processPayment,verification}=require("../controllers/razorpay");
 router.post(
     "/razorpay/:userId",
     requireSignin,
     isAuth,
     processPayment
+);
+router.post(
+    "/verification",
+    verification
 );
 
 router.param("userId",userById);
