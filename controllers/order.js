0a26f8b54b11c0436300b418c81b42b28f1c8a95
heli576 <helivakharia@gmail.com>
2020-10-02 20:41:19 +0530

    Add list orders for admin

--- controllers/order.js
+++ controllers/order.js
@@ -14,3 +14,17 @@ order.save((error,data)=>{
   res.json(data);
 })
 };
+
+exports.listOrders=(req,res)=>{
+  Order.find()
+  .populate('user',"_id name address")
+  .sort('-created')
+  .exec((err,orders)=>{
+    if(error){
+      return res.status(400).json({
+        error:errorHandler(error)
+      })
+    }
+    res.json(orders);
+  })
+}
