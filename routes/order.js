315f3e7bebcc0bc268fd998222e4d648d8bc12de
heli576 <helivakharia@gmail.com>
2020-10-05 20:18:16 +0530

    Add update status by admin

--- routes/order.js
+++ routes/order.js
@@ -2,12 +2,16 @@ const express=require("express");
 const router=express.Router();
 const {requireSignin,isAuth,isAdmin}=require("../controllers/auth");
 const {userById,addOrderToUserHistory}=require("../controllers/user");
-const {create,listOrders}=require("../controllers/order");
+const {create,listOrders,getStatusValues,orderById,updateOrderStatus}=require("../controllers/order");
 const {decreaseQuantity}=require("../controllers/product");
 
 router.post("/order/create/:userId",requireSignin,isAuth,addOrderToUserHistory,decreaseQuantity,create);
 router.get("/order/list/:userId",requireSignin,isAdmin,isAuth,listOrders);
+router.get("/order/status-values/:userId",requireSignin,isAdmin,isAuth,getStatusValues);
+router.put("/order/:orderId/status/:userId",requireSignin,isAdmin,isAuth,updateOrderStatus);
 router.param("userId",userById);
+router.param("orderId",orderById);
+
 
 
 module.exports=router;
